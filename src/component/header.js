67c7e8263a5175bd3a4da2a3a08d18606a979fd3
umesh <rajputumesh117@gmail.com>
2021-12-16 12:34:22 +0530

    my first react component

---
+++ src/component/header.js
@@ -0,0 +1,12 @@
+import logo from '../logo.svg';
+import '../css/style.css';
+
+function Header() {
+  return (
+    <div>
+      <h1>hello this is my first react component</h1>
+    </div>
+  );
+}
+
+export default Header;
