1245a2bd8071bc99742cea74e6ae3aaba5b58b04
umesh <rajputumesh117@gmail.com>
2021-12-23 12:33:52 +0530

    Footer changes

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -6,11 +6,12 @@ import '../css/bootstrap.min.css';
 class Footer extends Component {
   render() {
     return (<div id="">
-      <div className="">
+      <div className="contailer">
+        <a>@ all right reserved </a>
         <nav className="navbar navbar-expand-lg bg-white navbar-white">
           <div className="collapse navbar-collapse">
             <ul className="navbar-nav">
-              <li className="nav-item px-3">Home</li>
+              <li className="nav-item px-3"><i</li>
               <li className="nav-item px-3">About</li>
               <li className="nav-item px-3">Services</li>
               <li className="nav-item px-3">Gallery</li>
