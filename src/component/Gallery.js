a734eca0f611401e5e667a5f70af01ce7e4017d9
umesh <rajputumesh117@gmail.com>
2021-12-27 12:25:30 +0530

    react 1 or 2 app

--- src/component/Gallery.js
+++ src/component/Gallery.js
@@ -4,17 +4,15 @@ import { robots } from './robots.js';
 
 class Gallery extends Component {
   render() {
+    const products = robots.map((user , i) =>{
+      return <_Product username={robots[i].username} name={robots[i].name} email={robots[i].email} />
+    });
     return (<>
     <div className="container">
         <h2 className="text-center pt-4">Gallery</h2>
         <hr/>
         <div className="row my-3">
-            <_Product username={robots[0].username} name={robots[0].name} email={robots[0].email} />
-            <_Product username={robots[1].username} name={robots[1].name} email={robots[1].email} />
-            <_Product username={robots[2].username} name={robots[2].name} email={robots[2].email} />
-            <_Product username={robots[3].username} name={robots[3].name} email={robots[3].email} />
-            <_Product username={robots[4].username} name={robots[4].name} email={robots[4].email} />
-            <_Product username={robots[5].username} name={robots[5].name} email={robots[5].email} />
+            {products}
         </div>
     </div>
     </>);
