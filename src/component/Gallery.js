88f75f9a5ffaae1842500fa7547a72ab2daf1477
umesh <rajputumesh117@gmail.com>
2021-12-29 11:00:12 +0530

    building react app 3

--- src/component/Gallery.js
+++ src/component/Gallery.js
@@ -1,20 +1,45 @@
 import React, { Component } from 'react';
 import _Product from './product/_Product.js';
 import { robots } from './robots.js';
+import Search from './Search.js';
 
 class Gallery extends Component {
+  constructor()
+  {
+    super()
+    this.state = {
+      'robots': robots,
+      'searchFeild':''
+    }
+  }
+
+  searchRobots = (event) =>{
+    this.setState({'searchFeild':event.target.value});
+  }
+
   render() {
-    const products = robots.map((user , i) =>{
-      return <_Product username={robots[i].username} name={robots[i].name} email={robots[i].email} />
-    });
+      const filterrobots = this.state.robots.filter(robots => {
+        return robots.name.toLocaleLowerCase().includes(this.state.searchFeild.toLocaleLowerCase());
+      })
+      const products = filterrobots.map((user , i) =>{
+      return (<_Product 
+        key={i} 
+        id={filterrobots[i].id}
+        name={filterrobots[i].name}
+        email={filterrobots[i].email} />
+      );
+    })
     return (<>
-    <div className="container">
-        <h2 className="text-center pt-4">Gallery</h2>
-        <hr/>
-        <div className="row my-3">
-            {products}
-        </div>
-    </div>
+      <div className="container">
+          <h2 className="text-center pt-4">Gallery</h2>
+          <hr/>
+          <div>
+            <Search searchchange={this.searchRobots} />
+          </div>
+          <div className="row my-3">
+              {products}
+          </div>
+      </div>
     </>);
   }
 }
--- src/component/product/_Product.js
+++ src/component/product/_Product.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 
-const _Product = ({username, name, email}) => {
-    return (<div className="col-md-3 p-2">
-        <div className="p-2 m-1 bg-success border-radius">
-        <img className="img-fluid" alt="name" src={`https://robohash.org/${username}`} />
-        <p>{name}</p>
-        <p>{email}</p>
+const _Product = (robot) => {
+    return (<div className="col-md-3 p-2 text-center">
+        <div className="p-2 m-1 bg-light">
+        <img className="img-fluid" alt="name" src={`https://robohash.org/${robot.id}`} />
+        <div className="py-2">
+          <p>{robot.name}</p>
+          <p>{robot.email}</p>
+        </div>
         </div>
       </div>
     );
